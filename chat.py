754b6b07cd30ca6a0fdbd4002277de61fb294f46
RobertoFigueroa <fig18306@uvg.edu.gt>
2021-08-09 17:31:14 -0600

    [ADD] add contact done

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import getpass
+from ssl import PEM_cert_to_DER_cert
 
 from slixmpp import clientxmpp
 
@@ -8,34 +9,70 @@ from client import Client
 from aioconsole import ainput
 
 
-async def main(client : EchoBot):
-    print("-"*20)
-    print("1.Chat 1-2-1\n2.Other stuff\nAny number for exit")
-    opt = int(input("Choose an option\n->"))
-    if opt == 1:
-        name = await ainput("Write JID\n-> ")
-        client.set_im(name)
-        is_session_active = True
-        while is_session_active:
-            msg = await ainput("-->")
-            if msg != '\q' and len(msg)>0:
-                client.send_message(
-                    mto=name,
-                    mbody=msg,
-                    mtype='chat'
-                )
-            else:
-                is_session_active = False
+async def main(client : Client):
+    is_connected = True
+    while is_connected:
+        print("-"*20) #TODO: consider save this message on a conf. file
+        print("\t1.Chat 1-2-1\n \
+            2.Show roster\n \
+            3.Add friend\n \
+            4.Contact details\n \
+            5.Room chat\n \
+            6.Presence status\n \
+            7. Send notification\n \
+            8.Log out\n")
+        opt = int( await ainput("Choose an option\n->"))
+        if opt == 1:
+            name = await ainput("Write JID\n-> ")
+            client.set_im(name)
+            is_session_active = True
+            while is_session_active:
+                msg = await ainput("-->")
+                if msg != '\q' and len(msg)>0:
+                    client.send_message(
+                        mto=name,
+                        mbody=msg,
+                        mtype='chat'
+                    )
+                else:
+                    is_session_active = False
+        elif opt == 2:
+            await client.my_roster()
+        elif opt == 3:
+            fjid = await ainput("Friend's JID \n--> ")
+            print("Sending request ...")
+            client.send_presence_subscription(
+                pto=fjid,
+                pfrom=client.boundjid
+            )
+            print("Request sent!")
+        elif opt == 4:
+            pass
+        elif opt == 5:
+            pass
+        elif opt == 6:
+            pass
+        elif opt == 7:
+            pass
+        elif opt == 8:
+            is_connected = False
+            client.disconnect(reason='Not available, bye')
+        else:
+            pass
 
 if __name__ == "__main__":
+    
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format='%(levelname)-8s %(message)s')
+
     try:
         #TODO: manage input for registration or authentication
-        client = EchoBot("reg1@alumchat.xyz", "12345")
+        client = Client("reg1@alumchat.xyz", "12345")
+        print("Conectando ....")
         client.connect()
         client.loop.run_until_complete(client.connected_event.wait())
-        print("Conectando ....")
         client.loop.create_task(main(client))
-        client.process()
+        client.process(forever=False)
         #loop = asyncio.get_running_loop()
     except Exception as e:
         print("Error:", e)
--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+from os import EX_PROTOCOL
 import sys
 import asyncio
 import logging
@@ -8,40 +9,91 @@ from aioconsole import aprint
 
 import slixmpp
 from slixmpp.exceptions import IqError, IqTimeout
+import xml.etree.ElementTree as ET
 
 class Client(slixmpp.ClientXMPP):
     def __init__(self, jid, password, is_new=False):
         super().__init__(jid, password)
 
         self.im = None
-
-        self.connected_event = asyncio.Event()
         
+        self.recieved = set()
+        
+        self.connected_event = asyncio.Event()
+        self.presences_received = asyncio.Event()
+
         self.add_event_handler('session_start', self.start)
         self.add_event_handler('message', self.message)
         self.add_event_handler('register', self.register)
+        self.add_event_handler("changed_status", self.wait_for_presences)
         
         self.register_plugin('xep_0030') # Service Discovery
         self.register_plugin('xep_0004') # Data Forms
         self.register_plugin('xep_0060') # PubSub
         self.register_plugin('xep_0199') # Ping
+
         if is_new:
             self.register_plugin('xep_0004') # Data forms
             self.register_plugin('xep_0066') # Out-of-band Data
             self.register_plugin('xep_0077') # In-band Registration        
             self['xep_0077'].force_registration = True
 
-    async def chat_to(self, to, msg):
-        self.send_message(mto=to, mbody=msg, mtype='chat', mfrom=self.boundjid)
-        await asyncio.sleep(1)
-    def log_out(self):
-        self.disconnect(reason='Log out')
-
     def set_im(self, jid):
         self.im = jid
     
     def clear_im(self, jid):
         self.im = None
+
+    async def my_roster(self):
+        print("Waiting for presence updates ...\n")
+        await asyncio.sleep(10)
+
+        print('Roster for %s' % self.boundjid.bare)
+        groups = self.client_roster.groups()
+        for group in groups:
+            print('\n%s' % group)
+            print('-' * 72)
+            for jid in groups[group]:
+                sub = self.client_roster[jid]['subscription']
+                name = self.client_roster[jid]['name']
+                if self.client_roster[jid]['name']:
+                    print(' %s (%s) [%s]' % (name, jid, sub))
+                else:
+                    print(' %s [%s]' % (jid, sub))
+
+                connections = self.client_roster.presence(jid)
+                for res, pres in connections.items():
+                    show = 'available'
+                    if pres['show']:
+                        show = pres['show']
+                    print('   - %s (%s)' % (res, show))
+                    if pres['status']:
+                        print('       %s' % pres['status'])
+
+    async def add_contact(self, cjid):
+        resp =self.Iq()
+        resp['from'] = self.boundjid
+        resp['type'] = 'set'
+        resp['query'] = 'jabber:iq:roster'
+        payload = ET.fromstring("<query xmlns='jabber:iq:roster'><item jid='%s'><group>Friends</group></item></query>" % cjid)
+        resp.set_payload(payload)
+        try:
+            await resp.send()
+        except IqError as e:
+            logging.error("Could not add account: %s" 
+                    % e.iq['error']['text'])
+            aprint("Could not add account")
+        except IqTimeout:
+            logging.error("No response from server.")
+            aprint("Timeout, no response from server")
+        except:
+            print("Error while adding account")
+        #resp = self.Iq()
+        # resp['from'] = self.boundjid
+        # resp['type'] = 'set'
+        # resp['query']['xmlns'] = 'jabber:iq:roster'
+        # resp['query'][''] 
+        
 
     async def register(self, iq):
         resp = self.Iq()
@@ -68,11 +120,13 @@ class Client(slixmpp.ClientXMPP):
     #method for broadcast presence and retrieve roster when start session
     async def start(self, event):
         try:
-            self.send_presence(pstatus="Prueba el ecoooo") #<presence />
+            self.send_presence(pstatus="hola khe aze") #<presence />
             await self.get_roster() #IQ stanza for retrieve, response is saved by internal handler (self.roster)
             self.connected_event.set()
-        except:
-            print("An error has ocurred")
+        except IqError as err:
+            print('Error: %s' % err.iq['error']['condition'])
+        except IqTimeout:
+            print('Error: Request timed out')
 
     async def message(self, msg):
         if msg['type'] in ('normal', 'chat'):
@@ -83,6 +137,15 @@ class Client(slixmpp.ClientXMPP):
             #self.send_message(mto=msg['from'],
             #                        mbody="Thanks for sending:\n%s" % msg['body'])
 
+    def wait_for_presences(self, pres):
+            """
+            Track how many roster entries have received presence updates.
+            """
+            self.received.add(pres['from'].bare)
+            if len(self.received) >= len(self.client_roster.keys()):
+                self.presences_received.set()
+            else:
+                self.presences_received.clear()
 
 if __name__ == "__main__":
     optp = OptionParser()
@@ -111,7 +174,7 @@ if __name__ == "__main__":
     # ***
     # service discovery and ping
     # ***z
-    xmpp = EchoBot(opts.jid, opts.password)
+    xmpp = Client(opts.jid, opts.password)
     xmpp.register_plugin('xep_0030') # Service Discovery
     xmpp.register_plugin('xep_0199') # Ping
 
